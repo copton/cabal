ea66da0d11db3ad8f1ed9bdaac8de13db2bf54e3
Alexander Bernauer <copton@google.com>
2013-08-31 16:02:31 +0200

    Documentation and metadata

--- cabal-install/Distribution/Client/Manifest.hs
+++ cabal-install/Distribution/Client/Manifest.hs
@@ -1,23 +1,56 @@
-module Manifest (
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Distribution.Client.Manifest
+-- Copyright   :  (c) Google Inc., 2013
+-- License     :  BSD-like
+--
+-- Maintainer  :  Alexander Bernauer <copton@google.com>
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Maintain metadata of packages.
+--
+-- Needed to support uninstall.
+-----------------------------------------------------------------------------
+module Distribution.Client.Manifest (
+  -- * Data types
   Manifest(..),
-  writeManifest, readManifest, tryReadManifest, scanDirectory
+
+  -- * Functions
+  -- ** Reading and writing manifests.
+  writeManifest, readManifest, tryReadManifest,
+  -- ** utilities
+  scanDirectory
 ) where
 
-import Distribution.Package (InstalledPackageId)
-import Distribution.ParseUtils (simpleField, listField, parseFieldsFlat, ParseResult(..), showFilePath, parseFilePathQ, showFields, FieldDescr)
-import Distribution.Text (Text(disp, parse))
-import System.Directory (getDirectoryContents, doesDirectoryExist)
-import System.FilePath ((</>))
-import Text.PrettyPrint (empty)
+import Distribution.Package
+         ( InstalledPackageId )
+import Distribution.ParseUtils
+         ( simpleField, listField, parseFieldsFlat, ParseResult(..),
+           showFilePath, parseFilePathQ, showFields, FieldDescr )
+import Distribution.Text
+         ( Text(disp, parse) )
+import System.Directory
+         ( getDirectoryContents, doesDirectoryExist )
+import System.FilePath
+         ( (</>) )
+import Text.PrettyPrint
+         ( empty )
 
+-- | Metadata of a package
 data Manifest = Manifest
-  { libraryId :: Maybe InstalledPackageId
-  , manifestFiles :: [FilePath]
+  { libraryId :: Maybe InstalledPackageId  -- ^ If the cabal package contains a library, this is that library's ID
+  , manifestFiles :: [FilePath]  -- ^ The list of files copied to the file system when installing the package
   }
 
+-- | Write a manifest to disk.
 writeManifest :: Manifest -> FilePath -> IO ()
 writeManifest manifest file = writeFile file (showManifest manifest)
 
+-- | Read a manifest from disk.
+-- This function fails with 'error' in case of a parse error. This should actually never happen,
+-- since manifest files are only written by Cabal itself. See 'tryReadManifest' if you need to handle
+-- these cases separately.
 readManifest :: FilePath -> IO Manifest
 readManifest file = do
   manifest <- tryReadManifest file
@@ -25,9 +58,13 @@ readManifest file = do
     ParseOk _ manifest' -> return manifest'
     ParseFailed e ->  error (show e)  -- TODO: handle in a more appropriate way
 
+-- | Read a manifest from disk.
+-- This function returns a 'ParseResult', thus allowing to handle corrupt manifest files.
 tryReadManifest :: FilePath -> IO (ParseResult Manifest)
 tryReadManifest file = readFile file >>= return . parseManifest
 
+-- | Recursively collect all files within a directory.
+-- The returned file paths are relative to the given root.
 scanDirectory :: FilePath -> IO [FilePath]
 scanDirectory root = scan ""
   where
@@ -42,18 +79,22 @@ scanDirectory root = scan ""
       contents <- getDirectoryContents absolute
       return $ filter (\p -> p /= "." && p /= "..") contents
 
+-- | The actual manifest parser.
 parseManifest :: String -> ParseResult Manifest
 parseManifest = parseFieldsFlat fieldsManifest emptyManifest
 
+-- | A printer for manifests.
 showManifest :: Manifest -> String
 showManifest = showFields fieldsManifest
 
+-- | The empty manifest, used as initial state by the parser.
 emptyManifest :: Manifest
 emptyManifest = Manifest {
   libraryId = Nothing,
   manifestFiles = []
   }
 
+-- | Field descriptions of a manifest, used by the parser.
 fieldsManifest :: [FieldDescr Manifest]
 fieldsManifest = 
   [ simpleField "id" (maybe empty disp) (fmap Just parse) libraryId (\x r -> r {libraryId=x})
